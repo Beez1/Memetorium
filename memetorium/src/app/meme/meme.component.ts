93906008bde941feb432d0b32525da59bfde6cde
BeEz <98581184+Beez1@users.noreply.github.com>
2024-06-10 19:35:46 +0100

    Update code formatting and fix minor bugs

---
+++ memetorium/src/app/meme/meme.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-meme',
+  standalone: true,
+  imports: [],
+  templateUrl: './meme.component.html',
+  styleUrl: './meme.component.css'
+})
+export class MemeComponent {
+
+}
