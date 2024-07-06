a94e1f6bc023bb83077ac402908364cf2162bb2c
BeEz <98581184+Beez1@users.noreply.github.com>
2024-07-06 17:20:06 +0100

    removal of gallery sample

--- memetorium/src/app/view/view.component.ts
+++ memetorium/src/app/view/view.component.ts
@@ -6,17 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./view.component.css']
 })
 export class ViewComponent {
-  // Replace this with the provided image URL
-  imageUrl: string = 'https://image-processor-storage.s3.us-west-2.amazonaws.com/images/47f1a10c544f36028fc50e9b1ba77dbb/ras-al-khaimah-championship-day-one.jpg';
-  lightboxImageSrc: string = '';
-  lightboxVisible: boolean = false;
-
-  showImageInLightbox() {
-    this.lightboxImageSrc = this.imageUrl;
-    this.lightboxVisible = true;
-  }
-
-  closeLightbox() {
-    this.lightboxVisible = false;
-  }
+  title = 'memetorium';
 }
